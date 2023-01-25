6da9a2adf63c79b97303f0863c93bf3b42fa3a38
iurimolina <65170061+iurimolina@users.noreply.github.com>
2023-01-25 00:49:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 import os
 import time
 
@@ -11,12 +11,16 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+
+navegador.get("https://esaj.tjms.jus.br/esaj/portal.do?servico=740000")
 time.sleep(5)
-navegador.get("https://esaj.tjms.jus.br/sajcas/login?service=https%3A%2F%2Fesaj.tjms.jus.br%2Fesaj%2Fj_spring_cas_security_check")
+navegador.find_element('xpath', '//*[@id="identificacao"]/strong/a').click()
+time.sleep(5)
 navegador.find_element('xpath', '//*[@id="usernameForm"]').send_keys("52812952172", Keys.ARROW_DOWN)
 time.sleep(5)
 navegador.find_element('xpath', '//*[@id="passwordForm"]').send_keys("@Afonso4710", Keys.ARROW_DOWN)
 time.sleep(5)
 navegador.find_element('xpath', '//*[@id="pbEntrar"]').click()
 time.sleep(5)
-print("OKAYYYYYYYYYYYYYY")
+print("OK")
