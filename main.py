084034df82cc6f7d7ebbb3335bdc032d3c72ba7a
iurimolina <65170061+iurimolina@users.noreply.github.com>
2023-01-25 01:54:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 
-navegador.get("https://www.google.com.br/")
-navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("@esaj tjms", Keys.ARROW_DOWN)
-element = navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]')
-wait = WebDriverWait(navegador, 10)
-wait.until(lambda x: element.is_enabled())
-element.click()
-navegador.find_element('xpath', '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a/h3').click()
+navegador.get("https://www.tjms.jus.br/")
+
 
 print("OK")
