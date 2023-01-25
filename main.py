c1fe95e08a8b5e05f28a793ed914e6fd4afa511d
iurimolina <65170061+iurimolina@users.noreply.github.com>
2023-01-25 00:49:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import os
 import time
 
