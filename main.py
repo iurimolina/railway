6fb89c93b1c554a55296bc8271ab572b824ccb70
iurimolina <65170061+iurimolina@users.noreply.github.com>
2023-01-25 02:27:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
