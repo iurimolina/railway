dd2cc4500f05fdc085170df7a109c462a1d0a730
iurimolina <65170061+iurimolina@users.noreply.github.com>
2023-01-25 00:40:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import os
 import time
 
@@ -11,12 +11,12 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
+time.sleep(5)
 navegador.get("https://esaj.tjms.jus.br/sajcas/login?service=https%3A%2F%2Fesaj.tjms.jus.br%2Fesaj%2Fj_spring_cas_security_check")
 navegador.find_element('xpath', '//*[@id="usernameForm"]').send_keys("52812952172", Keys.ARROW_DOWN)
-#time.sleep(5)
+time.sleep(5)
 navegador.find_element('xpath', '//*[@id="passwordForm"]').send_keys("@Afonso4710", Keys.ARROW_DOWN)
-#time.sleep(5)
+time.sleep(5)
 navegador.find_element('xpath', '//*[@id="pbEntrar"]').click()
-#time.sleep(5)
-print("OK")
+time.sleep(5)
+print("OKAYYYYYYYYYYYYYY")
